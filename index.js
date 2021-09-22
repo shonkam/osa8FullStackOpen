b61c5aa09385cebb425b68bd44763ad7ffe43b76
shonkam <samu.honkamaki@gmail.com>
2021-09-23 00:33:37 +0300

    8.4

--- index.js
+++ index.js
@@ -103,7 +103,7 @@ const typeDefs = gql`
 type Query {
   bookCount: Int!
   authorCount: Int!
-  allBooks: [Book!]!
+  allBooks(author: String): [Book!]!
   allAuthors: [Author!]!
   }
 `
@@ -112,7 +112,7 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) => books.filter(p => p.author === args.author),
     allAuthors: () => authors
   },
   Author: {
